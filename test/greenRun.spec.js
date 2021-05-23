6d5f436017d6e55f955e8593b24c47c7a4b79888
kishorereddy514 <kishore514.reddy@gmail.com>
2021-05-22 23:18:24 -0400

    test scenarios to validate data

--- test/greenRun.spec.js
+++ test/greenRun.spec.js
@@ -1,31 +1,59 @@
 import supertest from 'supertest';
 import { expect } from 'chai';
-import {REQUEST, API_KEY} from './config';
+import { REQUEST, API_KEY } from './config';
 
-
-//const request = supertest('https://maps.googleapis.com/maps/api/geocode/');
-
-//const apiKey = 'AIzaSyBNW9ny7Q9TS1iRLYWgrWo4CwAb3wmrEik';
-//const invalidApiKey = 'testInvalid';
 
 //before block 
 before(async () => {
     //console.log('before test code goes here');
 });
 
-//Positive test with async 
+//Positive test 
 describe('Green Run - GET geocode with valid api key', () => {
-    it('Check status 200', async () => {
-        const res = await REQUEST.get('json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=' + API_KEY);
+    it('GeoCoding response', async () => {
+        const res = await REQUEST.get('address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=' + API_KEY);
         expect(res.statusCode).to.be.equals(200);
         expect(res.body.status).to.be.equals('OK');
     });
 
-    it('Reverse geocoding filtered with latlng', async () => {
-        const res = await REQUEST.get('json?latlng=40.714224,-73.961452&location_type=ROOFTOP&result_type=street_address&key=' + API_KEY);
+    it('Viewport biasing', async () => {
+        const res = await REQUEST.get('address=Winnetka&key=' + API_KEY);
         expect(res.statusCode).to.be.equals(200);
         expect(res.body.status).to.be.equals('OK');
         expect(res.body.results).to.not.be.length(0);
 
     });
+
+    it('Reverse geocoding filtered with latlng', async () => {
+        const res = await REQUEST.get('latlng=40.714224,-73.961452&location_type=ROOFTOP&result_type=street_address&key=' + API_KEY);
+        expect(res.statusCode).to.be.equals(200);
+        expect(res.body.status).to.be.equals('OK');
+        expect(res.body.results).to.not.be.length(0);
+
+    });
+
+
+});
+
+
+describe('Validate data ', () => {
+    let formatted_address_withOut_region, place_id_withOut_region;
+    it('Region biasing with Toledo', async () => {
+        const res = await REQUEST.get('address=Toledo&key=' + API_KEY);
+        expect(res.body.status).to.be.equals('OK');
+        expect(res.body.results).to.not.be.length(0);
+        //store formatted_address and place_id
+        formatted_address_withOut_region = res.body.results[0].formatted_address;
+        place_id_withOut_region = res.body.results[0].place_id;
+    });
+
+    it('Region biasing with Toledo with region=es', async () => {
+        const res = await REQUEST.get('address=Toledo&region=es&key=' + API_KEY);
+        expect(res.body.status).to.be.equals('OK');
+        expect(res.body.results).to.not.be.length(0);
+        // validate formatted_address with  region=es and without region 
+        expect(res.body.results[0].formatted_address).to.not.be.equals(formatted_address_withOut_region);
+        expect(res.body.results[0].place_id).to.not.be.equals(place_id_withOut_region);
+    });
+
 });
